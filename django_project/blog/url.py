234db1fd30661e065df145da40407f4305526512
KIM HANNA <93105056+kimhanna-git@users.noreply.github.com>
2021-12-22 23:03:38 +0900

    latest update

--- django_project/blog/url.py
+++ django_project/blog/url.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about') ##empty path.
-    # home view views.home and name is blog_home
+
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -6,8 +6,9 @@ def home(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render (request, 'blog/home.html', context)
+    return render(request, 'blog/home.html', context)
 
-def about(request) :
-    return render (request, 'blog/about.html', {'title': 'About'})
-##we need url pattern! ets creat urls.py and map the url for each view functionl
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
