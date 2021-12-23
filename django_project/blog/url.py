322ab6e2a608f771b40c61bdc4ba331f1569b908
KIM HANNA <93105056+kimhanna-git@users.noreply.github.com>
2021-12-23 19:14:18 +0900

    create post enabled

--- django_project/blog/url.py
+++ django_project/blog/url.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about') ##empty path.
 
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 def home(request):
     context = {
@@ -17,10 +18,13 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
 
-
-
-
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
