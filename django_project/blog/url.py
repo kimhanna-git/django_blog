985c3388ecff096b85c46c4921699fd28d7deb32
KIM HANNA <93105056+kimhanna-git@users.noreply.github.com>
2021-12-25 18:12:54 +0900

    post update delete enabled

--- django_project/blog/url.py
+++ django_project/blog/url.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView)
 from . import views
 
 
@@ -8,7 +12,9 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('about/', views.about, name='blog-about') ##empty path.
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about')
 
 ]
 
