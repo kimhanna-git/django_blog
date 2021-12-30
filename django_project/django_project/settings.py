cf1c1ea2aeaa7e5adce529ce6c2362b94fb583cd
KIM HANNA <93105056+kimhanna-git@users.noreply.github.com>
2021-12-30 22:08:34 +0900

    working but files missing

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -149,7 +149,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKET')
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3'
+
 
 
 
