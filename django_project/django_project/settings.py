8c5a92d4bd8eaf1acb145ca02123bd6d2579d5b3
kimhanna-git <kimhanna.contact@gmail.com>
2022-01-06 20:05:09 +0900

    new token generated, hid

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kt44b=j_pfk@!+y5=9%(+zmc)!c4h=4*-$al0=9-i%!s-!gqci'
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-kt44b=j_pfk@!+y5=9%(+zmc)!c4h=4*-$al0=9-i%!s-!gqci'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
