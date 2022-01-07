420be2c4f5955f0cf4696da2ad2e046bfc5818b8
kimhanna-git <kimhanna.contact@gmail.com>
2022-01-07 12:15:42 +0900

    os.environ[]

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('DJANGO_ACCESS')
+SECRET_KEY = os.environ['DJANGO_ACCESS']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
