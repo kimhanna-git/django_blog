b07686b826d6372438921706b574a902225efb29
kimhanna-git <kimhanna.contact@gmail.com>
2022-01-07 09:46:00 +0900

    debug test

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_ACCESS')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['hannarepublic.herokuapp.com']
