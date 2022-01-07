a0ce8dbbc4cfc93051993f57c080f321e1a18a1b
kimhanna-git <kimhanna.contact@gmail.com>
2022-01-07 12:38:29 +0900

    updated settings

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['hannarepublic.herokuapp.com']
+ALLOWED_HOSTS = ['hanna-blog.herokuapp.com']
 
 
 # Application definition
